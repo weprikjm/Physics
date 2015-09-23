145bfd6035c3b1fe6ff2f47d308089da74f8b5b8
David Roig <roig_96@hotmail.com>
2015-09-23 12:04:52 +0200

    .

--- Physics2_class 1/ModulePhysics.cpp
+++ Physics2_class 1/ModulePhysics.cpp
@@ -3,9 +3,18 @@
 #include "ModulePhysics.h"
 #include "math.h"
 
+#include "Box2D/Box2d/Box2D.h"
+#pragma comment (lib, "Box2D/libx86/Debug/Box2D.lib")
+
+#define GRAVITY_X 0.0f
+#define GRAVITY_Y -10.0f
+
+#define timeStep (float32) 1.0f / 60.f
+#define velocityIterations (int32) 10
+#define positionIterations (int32) 8
 // TODO 1: Include Box 2 header and library
 
-ModulePhysics::ModulePhysics(Application* app, bool start_enabled) : Module(app, start_enabled)
+ModulePhysics::ModulePhysics(Application* app, bool start_enabled) : Module(app, start_enabled), world(NULL), groundCircle(NULL)
 {
 	debug = true;
 }
@@ -17,14 +26,31 @@ ModulePhysics::~ModulePhysics()
 
 bool ModulePhysics::Start()
 {
+	b2Vec2 gravity(GRAVITY_X, GRAVITY_Y);
+
+	
 	LOG("Creating Physics 2D environment");
+	world = new b2World(gravity);
 
 	// TODO 2: Create a private variable for the world
 	// - You need to send it a default gravity
 	// - You need init the world in the constructor
 	// - Remember to destroy the world after using it
+	b2BodyDef circle_def;
+	circle_def.type = b2_dynamicBody;
+	circle_def.position.Set(0.0f, 2.0f);
 
+	groundCircle = world->CreateBody(&circle_def);
 
+	b2PolygonShape dynamicCircle;
+	dynamicCircle.SetAsBox(1.0f, 1.0f);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicCircle;
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+
+	groundCircle->CreateFixture(&fixtureDef);
 	// TODO 4: Create a a big static circle as "ground"
 	return true;
 }
@@ -33,11 +59,12 @@ bool ModulePhysics::Start()
 update_status ModulePhysics::PreUpdate()
 {
 	// TODO 3: Update the simulation ("step" the world)
-
+	world->Step(timeStep, velocityIterations, positionIterations);
+	world->ClearForces();
 	return UPDATE_CONTINUE;
 }
 
-// 
+
 update_status ModulePhysics::PostUpdate()
 {
 	// TODO 5: On space bar press, create a circle on mouse position
@@ -79,8 +106,9 @@ update_status ModulePhysics::PostUpdate()
 bool ModulePhysics::CleanUp()
 {
 	LOG("Destroying physics world");
-
 	// Delete the whole physics world!
-
+	world->DestroyBody(groundCircle);
+	delete(world);
+	
 	return true;
 }
--- Physics2_class 1/ModulePhysics.h
+++ Physics2_class 1/ModulePhysics.h
@@ -2,18 +2,22 @@
 #include "Module.h"
 #include "Globals.h"
 
+class b2World;
+class b2Body;
 class ModulePhysics : public Module
 {
 public:
+
 	ModulePhysics(Application* app, bool start_enabled = true);
 	~ModulePhysics();
-
+	
 	bool Start();
 	update_status PreUpdate();
 	update_status PostUpdate();
 	bool CleanUp();
 
 private:
-
+	b2World* world;
+	b2Body* groundCircle;
 	bool debug;
 };
